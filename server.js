9d6a790a46d504216c8e2a03b874220c809a498b
Aqsa Arshad <aqsa.arshad@bitverx.io>
2024-08-19 15:04:37 +0500

    db file removal

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-import { sequelize } from "./config/db.js";
+import { Sequelize } from "sequelize";
 import authRoutes from "./routes/auth.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+
+const sequelize = new Sequelize(process.env.POSTGRES_URI, {
+  dialect: 'postgres',
+});
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 await sequelize.sync();
 
