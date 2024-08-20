5475a1a38823ce0cb0262ce29288060e815765a7
AqsaArshad123 <aqsa.arshad@bitverx.io>
2024-08-20 12:32:08 +0500

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,3 +1,5 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') }); 
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
